bf3a750425edf85a9240f6512b40e1159eced917
KeplerBoyce <keplerboy@gmail.com>
2025-12-09 18:59:14 -0800

    Day 3 part 2 (messy)

--- bin/day03.ml
+++ bin/day03.ml
@@ -51,3 +51,53 @@ let () =
   let max_joltages = List.map get_max_joltage lines in
   let answer = List.fold_left (fun a x -> a + x) 0 max_joltages in
   print_endline (string_of_int answer)
+
+(* Part 2 *)
+let get_max_joltage2 total_digit_num bank =
+  let rec try_digit str so_far digit digit_num =
+    let len = String.length str in
+    if digit_num = total_digit_num then
+      (so_far, true)
+    else if len = 0 then
+      (so_far, false)
+    else if (total_digit_num - digit_num) > len then
+      (so_far, false)
+    else
+    
+    let c = Util.char_of_int digit in
+    let index =
+      match (String.index_from_opt str 0 c) with
+        | Some idx -> idx
+        | None -> -1
+    in
+    if index = -1 then
+      try_digit str so_far (digit - 1) digit_num
+    else
+
+    let new_so_far = so_far * 10 + digit in
+    let after_str = String.sub str (index + 1) (len - index - 1) in
+
+    let next_res = try_digit after_str new_so_far 9 (digit_num + 1) in
+    let (next_pos, next_full) = next_res in
+    if next_full then
+      next_res
+    else
+
+    if digit > 1 then
+      let same_res = try_digit str so_far (digit - 1) digit_num in
+      let (same_pos, _) = same_res in
+      if same_pos > next_pos then
+        same_res
+      else
+        next_res
+    else
+      next_res
+  in
+  let (joltage, _) = try_digit bank 0 9 0 in
+  joltage
+
+let () =
+  let lines = Util.read_lines "inputs/day03" in
+  let max_joltages = List.map (get_max_joltage2 12) lines in
+  let answer = List.fold_left (fun a x -> a + x) 0 max_joltages in
+  print_endline (string_of_int answer)
